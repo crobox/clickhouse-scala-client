d15a7fc609a5698fefe2f90e85d0bb529550949d
Nicu Reut <nicu@crobox.com>
2017-07-17 10:45:25 +0200

    Added date formatter for clickhouse day partition

---
+++ src/test/scala/com/crobox/clickhouse/partitioning/PartitionDateFormatterTest.scala
@@ -0,0 +1,22 @@
+package com.crobox.clickhouse.partitioning
+
+import java.util.Date
+
+import org.joda.time.DateTime
+import org.scalatest.{FlatSpecLike, Matchers}
+
+class PartitionDateFormatterTest extends FlatSpecLike with Matchers {
+
+  val expectedResult = "2017-12-31"
+  val inputTimestamp = 1514709779000L
+
+  it should "parse timestamp to partition date" in {
+    PartitionDateFormatter.dateFormat(inputTimestamp) should be(expectedResult)
+  }
+  it should "parse joda date time to partition date" in {
+    PartitionDateFormatter.dateFormat(new DateTime(inputTimestamp)) should be(expectedResult)
+  }
+  it should "parse java date to partition date" in {
+    PartitionDateFormatter.dateFormat(new Date(inputTimestamp)) should be(expectedResult)
+  }
+}
