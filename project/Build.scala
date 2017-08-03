cdb1cc5e84c3999e2bdffba2f2d6e040f3d30ffb
Sjoerd Mulder <sjoerdmulder@gmail.com>
2017-08-03 21:30:25 +0200

    Downgrade Jackson

--- project/Build.scala
+++ project/Build.scala
@@ -3,7 +3,7 @@ object Build {
 
 	val AkkaVersion = "2.4.19"
 	val AkkaHttpVersion = "10.0.9"
-	val JacksonVersion = "2.8.9"
-	val JacksonScalaVersion = "2.8.9"
+	val JacksonVersion = "2.7.9"
+	val JacksonScalaVersion = "2.7.9"
 
 }
