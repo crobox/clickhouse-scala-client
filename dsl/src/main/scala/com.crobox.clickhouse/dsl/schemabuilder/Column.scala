6311539938ecd038ad20c2e2166c303efc8a3962
Sjoerd Mulder <sjoerdmulder@gmail.com>
2018-02-16 20:56:06 +0100

    Updating types to have same characteristics as Scala

--- dsl/src/main/scala/com.crobox.clickhouse/dsl/schemabuilder/Column.scala
+++ dsl/src/main/scala/com.crobox.clickhouse/dsl/schemabuilder/Column.scala
@@ -19,41 +19,41 @@ object ColumnType {
 
   case object UInt8 extends SimpleColumnType("UInt8")
 
-  val Boolean = ColumnType.UInt8
+  val Boolean = UInt8
 
   case object UInt16 extends SimpleColumnType("UInt16")
 
-  val Short = UInt16
-
   case object UInt32 extends SimpleColumnType("UInt32")
 
-  val Int = ColumnType.UInt32
-
   case object UInt64 extends SimpleColumnType("UInt64")
-
-  val Long = ColumnType.UInt64
 
   case object Int8 extends SimpleColumnType("Int8")
 
   case object Int16 extends SimpleColumnType("Int16")
 
+  val Short = Int16
+
   case object Int32 extends SimpleColumnType("Int32")
 
+  val Int = Int32
+  
   case object Int64 extends SimpleColumnType("Int64")
 
+  val Long = Int64
+  
   case object Float32 extends SimpleColumnType("Float32")
 
-  val Float = ColumnType.Float32
+  val Float = Float32
 
   case object Float64 extends SimpleColumnType("Float64")
 
-  val Double = ColumnType.Float64
+  val Double = Float64
 
   case object String extends SimpleColumnType("String")
 
   case class FixedString(length: Int) extends SimpleColumnType(s"FixedString($length)")
 
-  val Uuid = ColumnType.String
+  val Uuid = String
 
   case object Date extends SimpleColumnType("Date")
 
