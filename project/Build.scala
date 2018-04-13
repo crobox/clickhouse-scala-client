c1a20973a917baeff8c76288ed005a6db9a77999
Nicu Reut <nicu@crobox.com>
2018-04-13 12:03:49 +0200

    Updated library version

--- project/Build.scala
+++ project/Build.scala
@@ -1,8 +1,8 @@
 import sbt._
 object Build {
 
-  val AkkaVersion     = "2.5.9"
-  val AkkaHttpVersion = "10.0.11"
+  val AkkaVersion     = "2.5.11"
+  val AkkaHttpVersion = "10.1.1"
   val JacksonVersion  = "2.7.9"
 
   val testDependencies = Seq("org.scalatest" %% "scalatest" % "3.0.0",
