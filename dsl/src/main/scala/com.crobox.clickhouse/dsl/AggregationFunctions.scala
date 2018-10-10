9fe7ed26e9e4aa28071d59126f6f931b0ca26f0b
Sjoerd Mulder <sjoerd@crobox.com>
2018-10-10 14:26:25 +0200

    Formatting doc

--- dsl/src/main/scala/com.crobox.clickhouse/dsl/AggregationFunctions.scala
+++ dsl/src/main/scala/com.crobox.clickhouse/dsl/AggregationFunctions.scala
@@ -36,16 +36,16 @@ object AggregateFunction {
       CombinedAggregatedFunction(CombinatorArray[T, Res](), aggregated)
 
     /**
-     * Having a column with type array, it aggregates all the results for that column by running the provided aggregation functions for each vertical slice of the array elements.
-     * Therefore, for the query result:
-     * \array_col|
-     * |[x1, y1, z1, u1]
-     * |[x2, y2, z2]
-     * |[x3, y3, z3]
-     *
-     * if you run sumForEach(array_col) you will get an array result with the following entries: [sum(x1,x3,x3), sum(y1,y2,y3), sum(z1, z2, z3), sum(u1)]
-     *
-     * */
+      * Having a column with type array, it aggregates all the results for that column by running the provided aggregation functions for each vertical slice of the array elements.
+      * Therefore, for the query result:
+      * \array_col|
+      * |[x1, y1, z1, u1]
+      * |[x2, y2, z2]
+      * |[x3, y3, z3]
+      *
+      * if you run sumForEach(array_col) you will get an array result with the following entries: [sum(x1,x3,x3), sum(y1,y2,y3), sum(z1, z2, z3), sum(u1)]
+      *
+      */
     def forEach[V, T <: TableColumn[Seq[V]], Res](
       column: T
     )(forEachFunc: TableColumn[V] => AggregateFunction[Res]): AggregateFunction[Seq[Res]] =
