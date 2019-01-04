e3b37f50c5e65ad15e9874bda7bff2bb22f63be4
Leonard Wolters <leonard@crobox.com>
2019-01-04 12:23:20 +0100

    Added additional parsing of DateTime

--- dsl/src/test/scala/com/crobox/clickhouse/dsl/marshalling/ClickhouseIntervalStartFormatTest.scala
+++ dsl/src/test/scala/com/crobox/clickhouse/dsl/marshalling/ClickhouseIntervalStartFormatTest.scala
@@ -15,6 +15,18 @@ class ClickhouseIntervalStartFormatTest extends FlatSpec with Matchers {
     ) should be(new DateTime("1970-04-01T00:00:00.000+02:00", DateTimeZone.UTC))
   }
 
+  it should "read using 0 as JsString" in {
+    ClickhouseIntervalStartFormat.read(JsString("0")) should be(
+      new DateTime("1970-01-01T00:00:00.000+00:00", DateTimeZone.UTC)
+    )
+  }
+
+  it should "read using 0 as JsNumber" in {
+    ClickhouseIntervalStartFormat.read(JsNumber(0)) should be(
+      new DateTime("1970-01-01T00:00:00.000+00:00", DateTimeZone.UTC)
+    )
+  }
+
   it should "read date only" in {
     ClickhouseIntervalStartFormat.read(JsString(s"1970-12-17_$zone")) should be(
       new DateTime("1970-12-17T00:00:00.000+02:00", DateTimeZone.UTC)
