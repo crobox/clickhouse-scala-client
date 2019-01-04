3fae8ac1ad59777a34a8e24e875e5bece9ac2ddc
Leonard Wolters <leonard@crobox.com>
2019-01-04 12:21:01 +0100

    Added additional parsing of DateTime

--- dsl/src/main/scala/com.crobox.clickhouse/dsl/marshalling/ClickhouseJsonSupport.scala
+++ dsl/src/main/scala/com.crobox.clickhouse/dsl/marshalling/ClickhouseJsonSupport.scala
@@ -50,13 +50,24 @@ trait ClickhouseJsonSupport {
                 .withZoneRetainFields(DateTimeZone.forID(timezoneId))
                 .withZone(DateTimeZone.UTC)
             case msTimestamp(millis) => new DateTime(millis.toLong, DateTimeZone.UTC)
-            case timestamp(secs) => new DateTime(secs.toLong * 1000, DateTimeZone.UTC)
-            case _ =>
-              try {
-                formatter.parseDateTime(value)
+            case timestamp(secs)     => new DateTime(secs.toLong * 1000, DateTimeZone.UTC)
+            case _                   =>
+              // sometimes clickhouse mistakenly returns a long / int value as JsString. Therefor, first try to
+              // parse it as a long...
+              val dateTime = try {
+                Option(new DateTime(value.toLong, DateTimeZone.UTC))
               } catch {
-                case _: IllegalArgumentException      => error(s"Couldn't parse $value into valid date time")
-                case _: UnsupportedOperationException => error("Unsupported operation, programmatic misconfiguration?")
+                case _: Throwable => None
+              }
+              // continue with parsing using the formatter
+              dateTime.getOrElse {
+                try {
+                  formatter.parseDateTime(value)
+                } catch {
+                  case _: IllegalArgumentException => error(s"Couldn't parse $value into valid date time")
+                  case _: UnsupportedOperationException =>
+                    error("Unsupported operation, programmatic misconfiguration?")
+                }
               }
           }
         case JsNumber(millis) => new DateTime(millis.longValue(), DateTimeZone.UTC)
