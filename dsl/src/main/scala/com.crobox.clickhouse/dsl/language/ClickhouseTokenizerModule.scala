14c147fec7d57846a26381a7630a1c92fa492b96
Sjoerd Mulder <sjoerd@crobox.com>
2020-03-27 09:49:27 +0100

    2.12 compat

--- dsl/src/main/scala/com.crobox.clickhouse/dsl/language/ClickhouseTokenizerModule.scala
+++ dsl/src/main/scala/com.crobox.clickhouse/dsl/language/ClickhouseTokenizerModule.scala
@@ -7,7 +7,7 @@ import com.typesafe.scalalogging.Logger
 import org.joda.time.{DateTime, DateTimeZone}
 import org.slf4j.LoggerFactory
 
-import scala.jdk.CollectionConverters._
+import scala.collection.JavaConverters._
 
 trait ClickhouseTokenizerModule
     extends TokenizerModule
