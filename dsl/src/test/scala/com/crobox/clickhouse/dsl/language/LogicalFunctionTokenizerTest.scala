6a88a790da63a5c299867febf49ac887cc1e4da4
Leonard Wolters <leonard@crobox.com>
2020-04-03 08:39:44 +0200

    Cleaning tests

--- dsl/src/test/scala/com/crobox/clickhouse/dsl/language/LogicalFunctionTokenizerTest.scala
+++ dsl/src/test/scala/com/crobox/clickhouse/dsl/language/LogicalFunctionTokenizerTest.scala
@@ -99,21 +99,21 @@ class LogicalFunctionTokenizerTest extends ClickhouseClientSpec with TestSchema 
   it should "add brackets triple AND" in {
     testQuery(
       Some(shieldId isEq "a" or ((shieldId isEq "b") and (shieldId isEq "c") and (shieldId isEq "d"))),
-      s"WHERE (shield_id = 'a' OR (shield_id = 'b' AND shield_id = 'c')) OR (not(shield_id = 'd') AND not(shield_id = 'e'))"
+      s"WHERE shield_id = 'a' OR ((shield_id = 'b' AND shield_id = 'c') AND shield_id = 'd')"
     )
   }
 
   it should "add brackets triple AND/OR" in {
     testQuery(
       Some(shieldId isEq "a" or ((shieldId isEq "b") and (shieldId isEq "c") or (shieldId isEq "d"))),
-      s"WHERE (shield_id = 'a' OR (shield_id = 'b' AND shield_id = 'c')) OR (not(shield_id = 'd') AND not(shield_id = 'e'))"
+      s"WHERE shield_id = 'a' OR ((shield_id = 'b' AND shield_id = 'c') OR shield_id = 'd')"
     )
   }
 
   it should "add brackets triple OR/AND" in {
     testQuery(
       Some(shieldId isEq "a" or ((shieldId isEq "b") or (shieldId isEq "c") and (shieldId isEq "d"))),
-      s"WHERE (shield_id = 'a' OR (shield_id = 'b' AND shield_id = 'c')) OR (not(shield_id = 'd') AND not(shield_id = 'e'))"
+      s"WHERE shield_id = 'a' OR ((shield_id = 'b' OR shield_id = 'c') AND shield_id = 'd')"
     )
   }
 
