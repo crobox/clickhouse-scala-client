595be666a33fec5c4ce69b11e386052267add587
Leonard Wolters <leonard@crobox.com>
2020-09-07 13:54:16 +0200

    Improved diffing

--- testkit/src/main/scala/com/crobox/clickhouse/testkit/ClickhouseMatchers.scala
+++ testkit/src/main/scala/com/crobox/clickhouse/testkit/ClickhouseMatchers.scala
@@ -11,11 +11,17 @@ trait ClickhouseMatchers {
       .replace(" )", ")")
       .trim
 
+  private def diff(s1: String, s2: String): String =
+    s1.zip(s2).map(tuple => if (tuple._1 == tuple._2) '_' else tuple._1).mkString("")
+
   def matchSQL(expected: String): Matcher[String] = new Matcher[String] {
 
     def apply(left: String): MatchResult =
-      MatchResult(clean(left) == clean(expected),
-                  s"SQL messages don't match. \nInput:     ${clean(left)}\n!= \nExpected:  ${clean(expected)}",
-                  "SQL messages are equal")
+      MatchResult(
+        clean(left) == clean(expected),
+        s"SQL messages don't match. \nInput:     ${clean(left)}\n!= \nExpected:  ${clean(expected)}" +
+          s"\nDIFF       ${diff(clean(left), clean(expected))}",
+        "SQL messages are equal"
+      )
   }
 }
