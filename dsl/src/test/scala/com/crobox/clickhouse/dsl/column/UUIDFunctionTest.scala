5b0f857169aab3378323ac7758271e1ab5d292ac
Leonard Wolters <leonard@crobox.com>
2020-09-17 11:28:06 +0200

    Added rewriting of notEmpty for UUID columns

--- dsl/src/test/scala/com/crobox/clickhouse/dsl/column/UUIDFunctionTest.scala
+++ dsl/src/test/scala/com/crobox/clickhouse/dsl/column/UUIDFunctionTest.scala
@@ -1,12 +1,16 @@
 package com.crobox.clickhouse.dsl.column
 
 import com.crobox.clickhouse.dsl._
-import com.crobox.clickhouse.{dsl => CHDsl}
 
 class UUIDFunctionTest extends ColumnFunctionTest {
 
   it should "rewrite notEmpty to notEquals(0)" in {
     var query = select(All()).from(OneTestTable).where(shieldId.notEmpty())
+    toSql(query.internalQuery, None) should matchSQL(
+      s"SELECT * FROM $database.captainAmerica WHERE shield_id != '0'"
+    )
+
+    query = select(All()).from(OneTestTable).where(notEmpty(shieldId))
     toSql(query.internalQuery, None) should matchSQL(
       s"SELECT * FROM $database.captainAmerica WHERE shield_id != '0'"
     )
