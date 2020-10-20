2676604d7dab3b4dfbf56e96a2f2de17d0de422c
Leonard Wolters <leonard@crobox.com>
2020-10-20 18:52:11 +0200

    Improving query reducing

--- dsl/src/main/scala/com.crobox.clickhouse/dsl/language/LogicalFunctionTokenizer.scala
+++ dsl/src/main/scala/com.crobox.clickhouse/dsl/language/LogicalFunctionTokenizer.scala
@@ -13,13 +13,35 @@ trait LogicalFunctionTokenizer {
       case (Some(left), Some(right)) =>
         col.operator match {
           case And =>
-            if (left.isConstTrue) tokenizeColumn(right) // LEFT is true, only tokenizer RIGHT
+            if (left.isConstFalse || right.isConstFalse) "0" // LEFT or RIGHT is false; AND fails
+            else if (left.isConstTrue) tokenizeColumn(right) // LEFT is true, only tokenizer RIGHT
             else if (right.isConstTrue) tokenizeColumn(left) // RIGHT is true, only tokenizer LEFT
-            else s"${tokenize(left, col.operator)} AND ${tokenize(right, col.operator)}"
+            else {
+              //s"${tokenize(left, col.operator)} AND ${tokenize(right, col.operator)}"
+              (tokenize(left, col.operator), tokenize(right, col.operator)) match {
+                case ("1", "1")                => "1" // LEFT & RIGHT are true, AND succeeds
+                case ("1", rightClause)        => rightClause // LEFT is true, only tokenize RIGHT
+                case ("0", _)                  => "0" // LEFT is false, AND fails
+                case (leftClause, "1")         => leftClause // RIGHT is true, only tokenize LEFT
+                case (_, "0")                  => "0" // RIGHT is false, AND fails
+                case (leftClause, rightClause) => s"$leftClause AND $rightClause"
+              }
+            }
           case Or =>
-            if (left.isConstFalse) tokenizeColumn(right) // LEFT is false, only tokenize right
+            if (left.isConstTrue || right.isConstTrue) "1" // LEFT or RIGHT is true; OR succeeds
+            else if (left.isConstFalse) tokenizeColumn(right) // LEFT is false, only tokenize right
             else if (right.isConstFalse) tokenizeColumn(left) // RIGHT is false, only tokenize left
-            else s"${tokenize(left, col.operator)} OR ${tokenize(right, col.operator)}"
+            else {
+              s"${tokenize(left, col.operator)} OR ${tokenize(right, col.operator)}"
+              (tokenize(left, col.operator), tokenize(right, col.operator)) match {
+                case ("0", "0")                => "0" // LEFT & RIGHT are false, OR fails
+                case ("0", rightClause)        => rightClause // LEFT is false, only tokenize RIGHT
+                case ("1", _)                  => "1" // LEFT is true, OR succeeds
+                case (leftClause, "0")         => leftClause // RIGHT is false, only tokenize LEFT
+                case (_, "1")                  => "1" // RIGHT is true, OR succeeds
+                case (leftClause, rightClause) => s"$leftClause OR $rightClause"
+              }
+            }
           case Xor => s"xor(${tokenize(left, col.operator)}, ${tokenize(right, col.operator)})"
           case Not => s"not(${tokenize(left, col.operator)})"
         }
