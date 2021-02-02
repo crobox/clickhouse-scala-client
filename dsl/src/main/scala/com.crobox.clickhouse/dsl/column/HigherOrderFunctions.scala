d680e69c6d6a1e1a345713c06de4f45ad312ecfa
Leonard Wolters <leonard@crobox.com>
2021-02-02 09:35:42 +0100

    Implementing higher level order functions (to level 3)

--- dsl/src/main/scala/com.crobox.clickhouse/dsl/column/HigherOrderFunctions.scala
+++ dsl/src/main/scala/com.crobox.clickhouse/dsl/column/HigherOrderFunctions.scala
@@ -10,41 +10,160 @@ trait HigherOrderFunctions { self: Magnets =>
       val arrays: ArrayColMagnet[_ <: Iterable[I]]*
   ) extends ExpressionColumn[R](EmptyColumn)
 
+  // double type casts
+  case class ArrayAll[I, O](_func1: Option[TableColumn[I] => ExpressionColumn[O]],
+                            _func2: Option[(TableColumn[I], TableColumn[I]) => ExpressionColumn[O]],
+                            _func3: Option[(TableColumn[I], TableColumn[I], TableColumn[I]) => ExpressionColumn[O]],
+                            _arrays: ArrayColMagnet[_ <: Iterable[I]]*)
+      extends HigherOrderFunction[I, O, Iterable[I]](_func1, _func2, _func3, _arrays: _*)
+  case class ArrayCumSum[I, O](_func1: Option[TableColumn[I] => ExpressionColumn[O]],
+                               _func2: Option[(TableColumn[I], TableColumn[I]) => ExpressionColumn[O]],
+                               _func3: Option[(TableColumn[I], TableColumn[I], TableColumn[I]) => ExpressionColumn[O]],
+                               _arrays: ArrayColMagnet[_ <: Iterable[I]]*)
+      extends HigherOrderFunction[I, O, Iterable[I]](_func1, _func2, _func3, _arrays: _*)
   case class ArrayMap[I, O](_func1: Option[TableColumn[I] => ExpressionColumn[O]],
                             _func2: Option[(TableColumn[I], TableColumn[I]) => ExpressionColumn[O]],
                             _func3: Option[(TableColumn[I], TableColumn[I], TableColumn[I]) => ExpressionColumn[O]],
                             _arrays: ArrayColMagnet[_ <: Iterable[I]]*)
       extends HigherOrderFunction[I, O, Iterable[I]](_func1, _func2, _func3, _arrays: _*)
-  case class ArrayFilter[I](_func: TableColumn[I] => ExpressionColumn[Boolean],
-                            _array: ArrayColMagnet[_ <: Iterable[I]])
-      extends HigherOrderFunction[I, Boolean, Iterable[I]](Some(_func), None, None, _array)
-  case class ArrayCount[I](_func: Option[TableColumn[I] => ExpressionColumn[Boolean]],
-                           _array: ArrayColMagnet[_ <: Iterable[I]])
-      extends HigherOrderFunction[I, Boolean, Long](_func, None, None, _array)
-  case class ArrayExists[I](_func: Option[TableColumn[I] => ExpressionColumn[Boolean]],
-                            _array: ArrayColMagnet[_ <: Iterable[I]])
-      extends HigherOrderFunction[I, Boolean, Boolean](_func, None, None, _array)
-  case class ArrayAll[I, O](_func: Option[TableColumn[I] => ExpressionColumn[O]],
-                            _array: ArrayColMagnet[_ <: Iterable[I]])
-      extends HigherOrderFunction[I, O, Boolean](_func, None, None, _array)
-  case class ArraySum[I, O](_func: Option[TableColumn[I] => ExpressionColumn[O]],
-                            _array: ArrayColMagnet[_ <: Iterable[I]])
-      extends HigherOrderFunction[I, O, Long](_func, None, None, _array)
-  case class ArrayFirst[I](_func: Option[TableColumn[I] => ExpressionColumn[Boolean]],
-                           _array: ArrayColMagnet[_ <: Iterable[I]])
-      extends HigherOrderFunction[I, Boolean, I](_func, None, None, _array)
-  case class ArrayFirstIndex[I](_func: Option[TableColumn[I] => ExpressionColumn[Boolean]],
-                                _array: ArrayColMagnet[_ <: Iterable[I]])
-      extends HigherOrderFunction[I, Boolean, Long](_func, None, None, _array)
-  case class ArrayCumSum[I, O](_func: Option[TableColumn[I] => ExpressionColumn[O]],
-                               _array: ArrayColMagnet[_ <: Iterable[I]])
-      extends HigherOrderFunction[I, O, Iterable[Long]](_func, None, None, _array)
-  case class ArraySort[I, O](_func: Option[TableColumn[I] => ExpressionColumn[O]],
-                             _array: ArrayColMagnet[_ <: Iterable[I]])
-      extends HigherOrderFunction[I, O, Iterable[I]](_func, None, None, _array)
-  case class ArrayReverseSort[I, O](_func: Option[TableColumn[I] => ExpressionColumn[O]],
-                                    _array: ArrayColMagnet[_ <: Iterable[I]])
-      extends HigherOrderFunction[I, O, Iterable[I]](_func, None, None, _array)
+  case class ArrayReverseSort[I, O](
+      _func1: Option[TableColumn[I] => ExpressionColumn[O]],
+      _func2: Option[(TableColumn[I], TableColumn[I]) => ExpressionColumn[O]],
+      _func3: Option[(TableColumn[I], TableColumn[I], TableColumn[I]) => ExpressionColumn[O]],
+      _arrays: ArrayColMagnet[_ <: Iterable[I]]*
+  ) extends HigherOrderFunction[I, O, Iterable[I]](_func1, _func2, _func3, _arrays: _*)
+  case class ArraySort[I, O](_func1: Option[TableColumn[I] => ExpressionColumn[O]],
+                             _func2: Option[(TableColumn[I], TableColumn[I]) => ExpressionColumn[O]],
+                             _func3: Option[(TableColumn[I], TableColumn[I], TableColumn[I]) => ExpressionColumn[O]],
+                             _arrays: ArrayColMagnet[_ <: Iterable[I]]*)
+      extends HigherOrderFunction[I, O, Iterable[I]](_func1, _func2, _func3, _arrays: _*)
+  case class ArraySum[I, O](_func1: Option[TableColumn[I] => ExpressionColumn[O]],
+                            _func2: Option[(TableColumn[I], TableColumn[I]) => ExpressionColumn[O]],
+                            _func3: Option[(TableColumn[I], TableColumn[I], TableColumn[I]) => ExpressionColumn[O]],
+                            _arrays: ArrayColMagnet[_ <: Iterable[I]]*)
+      extends HigherOrderFunction[I, O, Iterable[I]](_func1, _func2, _func3, _arrays: _*)
+
+  // single type casts
+  case class ArrayCount[I](
+      _func1: Option[TableColumn[I] => ExpressionColumn[Boolean]],
+      _func2: Option[(TableColumn[I], TableColumn[I]) => ExpressionColumn[Boolean]],
+      _func3: Option[(TableColumn[I], TableColumn[I], TableColumn[I]) => ExpressionColumn[Boolean]],
+      _arrays: ArrayColMagnet[_ <: Iterable[I]]*
+  ) extends HigherOrderFunction[I, Boolean, Iterable[I]](_func1, _func2, _func3, _arrays: _*)
+  case class ArrayExists[I](
+      _func1: Option[TableColumn[I] => ExpressionColumn[Boolean]],
+      _func2: Option[(TableColumn[I], TableColumn[I]) => ExpressionColumn[Boolean]],
+      _func3: Option[(TableColumn[I], TableColumn[I], TableColumn[I]) => ExpressionColumn[Boolean]],
+      _arrays: ArrayColMagnet[_ <: Iterable[I]]*
+  ) extends HigherOrderFunction[I, Boolean, Iterable[I]](_func1, _func2, _func3, _arrays: _*)
+  case class ArrayFilter[I](
+      _func1: Option[TableColumn[I] => ExpressionColumn[Boolean]],
+      _func2: Option[(TableColumn[I], TableColumn[I]) => ExpressionColumn[Boolean]],
+      _func3: Option[(TableColumn[I], TableColumn[I], TableColumn[I]) => ExpressionColumn[Boolean]],
+      _arrays: ArrayColMagnet[_ <: Iterable[I]]*
+  ) extends HigherOrderFunction[I, Boolean, Iterable[I]](_func1, _func2, _func3, _arrays: _*)
+  case class ArrayFirst[I](
+      _func1: Option[TableColumn[I] => ExpressionColumn[Boolean]],
+      _func2: Option[(TableColumn[I], TableColumn[I]) => ExpressionColumn[Boolean]],
+      _func3: Option[(TableColumn[I], TableColumn[I], TableColumn[I]) => ExpressionColumn[Boolean]],
+      _arrays: ArrayColMagnet[_ <: Iterable[I]]*
+  ) extends HigherOrderFunction[I, Boolean, Iterable[I]](_func1, _func2, _func3, _arrays: _*)
+  case class ArrayFirstIndex[I](
+      _func1: Option[TableColumn[I] => ExpressionColumn[Boolean]],
+      _func2: Option[(TableColumn[I], TableColumn[I]) => ExpressionColumn[Boolean]],
+      _func3: Option[(TableColumn[I], TableColumn[I], TableColumn[I]) => ExpressionColumn[Boolean]],
+      _arrays: ArrayColMagnet[_ <: Iterable[I]]*
+  ) extends HigherOrderFunction[I, Boolean, Iterable[I]](_func1, _func2, _func3, _arrays: _*)
+
+  def arrayAll[I](func: TableColumn[I] => ExpressionColumn[Boolean],
+                  array: ArrayColMagnet[_ <: Iterable[I]]): ArrayAll[I, Boolean] =
+    ArrayAll(Option(func), None, None, array)
+
+  def arrayAll2[I](func: (TableColumn[I], TableColumn[I]) => ExpressionColumn[Boolean],
+                   array1: ArrayColMagnet[_ <: Iterable[I]],
+                   array2: ArrayColMagnet[_ <: Iterable[I]]): ArrayAll[I, Boolean] =
+    ArrayAll(None, Option(func), None, array1, array2)
+
+  def arrayAll3[I](func: (TableColumn[I], TableColumn[I], TableColumn[I]) => ExpressionColumn[Boolean],
+                   array1: ArrayColMagnet[_ <: Iterable[I]],
+                   array2: ArrayColMagnet[_ <: Iterable[I]],
+                   array3: ArrayColMagnet[_ <: Iterable[I]]): ArrayAll[I, Boolean] =
+    ArrayAll(None, None, Option(func), array1, array2, array3)
+
+  def arrayCount[I](func: Option[TableColumn[I] => ExpressionColumn[Boolean]],
+                    array: ArrayColMagnet[_ <: Iterable[I]]): ArrayCount[I] =
+    ArrayCount(func, None, None, array)
+
+  def arrayCount2[I](func: (TableColumn[I], TableColumn[I]) => ExpressionColumn[Boolean],
+                     array1: ArrayColMagnet[_ <: Iterable[I]],
+                     array2: ArrayColMagnet[_ <: Iterable[I]]): ArrayCount[I] =
+    ArrayCount(None, Option(func), None, array1, array2)
+
+  def arrayCount3[I](func: (TableColumn[I], TableColumn[I], TableColumn[I]) => ExpressionColumn[Boolean],
+                     array1: ArrayColMagnet[_ <: Iterable[I]],
+                     array2: ArrayColMagnet[_ <: Iterable[I]],
+                     array3: ArrayColMagnet[_ <: Iterable[I]]): ArrayCount[I] =
+    ArrayCount(None, None, Option(func), array1, array2, array3)
+
+  def arrayExists[I](func: TableColumn[I] => ExpressionColumn[Boolean],
+                     array: ArrayColMagnet[_ <: Iterable[I]]): ArrayExists[I] =
+    ArrayExists(Option(func), None, None, array)
+
+  def arrayExists2[I](func: (TableColumn[I], TableColumn[I]) => ExpressionColumn[Boolean],
+                      array1: ArrayColMagnet[_ <: Iterable[I]],
+                      array2: ArrayColMagnet[_ <: Iterable[I]]): ArrayExists[I] =
+    ArrayExists(None, Option(func), None, array1, array2)
+
+  def arrayExists3[I](func: (TableColumn[I], TableColumn[I], TableColumn[I]) => ExpressionColumn[Boolean],
+                      array1: ArrayColMagnet[_ <: Iterable[I]],
+                      array2: ArrayColMagnet[_ <: Iterable[I]],
+                      array3: ArrayColMagnet[_ <: Iterable[I]]): ArrayExists[I] =
+    ArrayExists(None, None, Option(func), array1, array2, array3)
+
+  def arrayFilter[I](func: TableColumn[I] => ExpressionColumn[Boolean],
+                     array: ArrayColMagnet[_ <: Iterable[I]]): ArrayFilter[I] =
+    ArrayFilter(Option(func), None, None, array)
+
+  def arrayFilter2[I](func: (TableColumn[I], TableColumn[I]) => ExpressionColumn[Boolean],
+                      array1: ArrayColMagnet[_ <: Iterable[I]],
+                      array2: ArrayColMagnet[_ <: Iterable[I]]): ArrayFilter[I] =
+    ArrayFilter(None, Option(func), None, array1, array2)
+
+  def arrayFilter3[I](func: (TableColumn[I], TableColumn[I], TableColumn[I]) => ExpressionColumn[Boolean],
+                      array1: ArrayColMagnet[_ <: Iterable[I]],
+                      array2: ArrayColMagnet[_ <: Iterable[I]],
+                      array3: ArrayColMagnet[_ <: Iterable[I]]): ArrayFilter[I] =
+    ArrayFilter(None, None, Option(func), array1, array2, array3)
+
+  def arrayFirst[I](func: TableColumn[I] => ExpressionColumn[Boolean],
+                    array: ArrayColMagnet[_ <: Iterable[I]]): ArrayFirst[I] =
+    ArrayFirst(Option(func), None, None, array)
+
+  def arrayFirst2[I](func: (TableColumn[I], TableColumn[I]) => ExpressionColumn[Boolean],
+                     array1: ArrayColMagnet[_ <: Iterable[I]],
+                     array2: ArrayColMagnet[_ <: Iterable[I]]): ArrayFirst[I] =
+    ArrayFirst(None, Option(func), None, array1, array2)
+
+  def arrayFirst3[I](func: (TableColumn[I], TableColumn[I], TableColumn[I]) => ExpressionColumn[Boolean],
+                     array1: ArrayColMagnet[_ <: Iterable[I]],
+                     array2: ArrayColMagnet[_ <: Iterable[I]],
+                     array3: ArrayColMagnet[_ <: Iterable[I]]): ArrayFirst[I] =
+    ArrayFirst(None, None, Option(func), array1, array2, array3)
+
+  def arrayFirstIndex[I](func: TableColumn[I] => ExpressionColumn[Boolean],
+                         array: ArrayColMagnet[_ <: Iterable[I]]): ArrayFirstIndex[I] =
+    ArrayFirstIndex(Option(func), None, None, array)
+
+  def arrayFirstIndex2[I](func: (TableColumn[I], TableColumn[I]) => ExpressionColumn[Boolean],
+                          array1: ArrayColMagnet[_ <: Iterable[I]],
+                          array2: ArrayColMagnet[_ <: Iterable[I]]): ArrayFirstIndex[I] =
+    ArrayFirstIndex(None, Option(func), None, array1, array2)
+
+  def arrayFirstIndex3[I](func: (TableColumn[I], TableColumn[I], TableColumn[I]) => ExpressionColumn[Boolean],
+                          array1: ArrayColMagnet[_ <: Iterable[I]],
+                          array2: ArrayColMagnet[_ <: Iterable[I]],
+                          array3: ArrayColMagnet[_ <: Iterable[I]]): ArrayFirstIndex[I] =
+    ArrayFirstIndex(None, None, Option(func), array1, array2, array3)
 
   def arrayMap[I, O](func: TableColumn[I] => ExpressionColumn[O],
                      array: ArrayColMagnet[_ <: Iterable[I]]): ArrayMap[I, O] =
@@ -61,43 +180,19 @@ trait HigherOrderFunctions { self: Magnets =>
                       array3: ArrayColMagnet[_ <: Iterable[I]]): ArrayMap[I, O] =
     ArrayMap(None, None, Option(func), array1, array2, array3)
 
-  def arrayFilter[I](func: TableColumn[I] => ExpressionColumn[Boolean],
-                     array: ArrayColMagnet[_ <: Iterable[I]]): ArrayFilter[I] =
-    ArrayFilter(func, array)
-
-  def arrayCount[I](func: Option[TableColumn[I] => ExpressionColumn[Boolean]],
-                    array: ArrayColMagnet[_ <: Iterable[I]]): ArrayCount[I] =
-    ArrayCount(func, array)
-
-  def arrayExists[I](func: TableColumn[I] => ExpressionColumn[Boolean],
-                     array: ArrayColMagnet[_ <: Iterable[I]]): ArrayExists[I] =
-    ArrayExists[I](Some(func), array)
-
-  def arrayAll[I](func: TableColumn[I] => ExpressionColumn[Boolean],
-                  array: ArrayColMagnet[_ <: Iterable[I]]): ArrayAll[I, Boolean] =
-    ArrayAll(Some(func), array)
-
   def arraySum[I, O](func: Option[TableColumn[I] => ExpressionColumn[O]],
                      array: ArrayColMagnet[_ <: Iterable[I]]): ArraySum[I, O] =
-    ArraySum(func, array)
-
-  def arrayFirst[I](func: TableColumn[I] => ExpressionColumn[Boolean],
-                    array: ArrayColMagnet[_ <: Iterable[I]]): ArrayFirst[I] =
-    ArrayFirst(Some(func), array)
-
-  def arrayFirstIndex[I](func: TableColumn[I] => ExpressionColumn[Boolean],
-                         array: ArrayColMagnet[_ <: Iterable[I]]): ArrayFirstIndex[I] =
-    ArrayFirstIndex(Some(func), array)
+    ArraySum(func, None, None, array)
 
   def arrayCumSum[I, O](func: Option[TableColumn[I] => ExpressionColumn[O]],
                         array: ArrayColMagnet[_ <: Iterable[I]]): ArrayCumSum[I, O] =
-    ArrayCumSum(func, array)
+    ArrayCumSum(func, None, None, array)
 
   def arraySort[I, O](func: Option[TableColumn[I] => ExpressionColumn[O]],
                       array: ArrayColMagnet[_ <: Iterable[I]]): ArraySort[I, O] =
-    ArraySort(func, array)
+    ArraySort(func, None, None, array)
 
   def arrayReverseSort[I, O](func: Option[TableColumn[I] => ExpressionColumn[O]],
                              array: ArrayColMagnet[_ <: Iterable[I]]): ArrayReverseSort[I, O] =
-    ArrayReverseSort(func, array)
+    ArrayReverseSort(func, None, None, array)
 }
--- dsl/src/test/scala/com/crobox/clickhouse/dsl/column/HigherOrderFunctionsTest.scala
+++ dsl/src/test/scala/com/crobox/clickhouse/dsl/column/HigherOrderFunctionsTest.scala
@@ -7,18 +7,18 @@ class HigherOrderFunctionsTest extends ColumnFunctionTest {
   "Tokenization" should "succeed for HigherOrderFunctions" in {
     val arr1 = Seq(1L, 2L, 3L)
 
-    r(arrayMap[Long, Long](x => x * 2L, arr1)) shouldBe "[2,4,6]"
-    r(arrayFilter[Long](_ <> 2L, arr1)) shouldBe "[1,3]"
-    r(arrayExists[Long](_.isEq(2L), arr1)) shouldBe "1"
     r(arrayAll[Long](_ <= 3, arr1)) shouldBe "1"
     r(arrayAll[Long](_.isEq(2L), arr1)) shouldBe "0"
-    r(arrayFirst[Long](modulo(_, 2L).isEq(0), arr1)) shouldBe "2"
-    r(arrayFirstIndex[Long](modulo(_, 2L).isEq(0), arr1)) shouldBe "2"
-    r(arraySum[Long, Long](Some(_ * 2L), arr1)) shouldBe "12"
     r(arrayCount[Long](Some(_.isEq(2L)), arr1)) shouldBe "1"
     r(arrayCumSum[Long, Long](Some(_ * 2L), arr1)) shouldBe "[2,6,12]"
-    r(arraySort[Long, Double](Some(_     % 3.0), arr1)) shouldBe "[3,1,2]"
+    r(arrayExists[Long](_.isEq(2L), arr1)) shouldBe "1"
+    r(arrayFilter[Long](_ <> 2L, arr1)) shouldBe "[1,3]"
+    r(arrayFirst[Long](modulo(_, 2L).isEq(0), arr1)) shouldBe "2"
+    r(arrayFirstIndex[Long](modulo(_, 2L).isEq(0), arr1)) shouldBe "2"
+    r(arrayMap[Long, Long](x => x * 2L, arr1)) shouldBe "[2,4,6]"
     r(arrayReverseSort[Long, Int](Some(_ % 3), arr1)) shouldBe "[2,1,3]"
+    r(arraySort[Long, Double](Some(_     % 3.0), arr1)) shouldBe "[3,1,2]"
+    r(arraySum[Long, Long](Some(_ * 2L), arr1)) shouldBe "12"
 
     // Second Order higher order
     r(arrayMap2[Long, Long]((x, y) => x * y, arr1, arr1)) shouldBe "[1,4,9]"
