0df43cdaf4dc8e34bc452f99d19f2bb91d428f6d
Leonard Wolters <leonard@crobox.com>
2021-02-04 20:24:14 +0100

    New version

--- dsl/src/test/scala/com/crobox/clickhouse/dsl/column/HigherOrderFunctionsTest.scala
+++ dsl/src/test/scala/com/crobox/clickhouse/dsl/column/HigherOrderFunctionsTest.scala
@@ -10,10 +10,10 @@ class HigherOrderFunctionsTest extends ColumnFunctionTest {
     r(arrayAll[Long](_.isEq(2L), arr1)) shouldBe "0"
   }
 
-//  it should "HigherOrderFunctions: arrayAvg" in {
-//    r(arrayAvg[Long, Long](None, arr1)) shouldBe "2"
-//    r(arrayAvg[Long, Double](Option(x => x * 333d), arr1)) shouldBe "0"
-//  }
+  it should "HigherOrderFunctions: arrayAvg" in {
+    r(arrayAvg[Long, Long](None, arr1)) shouldBe "2"
+    r(arrayAvg[Long, Double](Option(x => x * 333d), arr1)) shouldBe "666"
+  }
 
   it should "HigherOrderFunctions: arrayCount" in {
     r(arrayCount[Long](Some(_.isEq(2L)), arr1)) shouldBe "1"
@@ -56,15 +56,15 @@ class HigherOrderFunctionsTest extends ColumnFunctionTest {
     r(arrayMap3[Long, Long]((x, y, z) => x * y * z, arr1, arr1, arr1)) shouldBe "[1,8,27]"
   }
 
-//  it should "HigherOrderFunctions: arrayMax" in {
-//    r(arrayMax[Long, Long](None, arr1)) shouldBe "3"
-//    r(arrayMax[Long, Long](Option(x => x * -1L), arr1)) shouldBe "1"
-//  }
-//
-//  it should "HigherOrderFunctions: arrayMin" in {
-//    r(arrayMin[Long, Long](None, arr1)) shouldBe "1"
-//    r(arrayMin[Long, Long](Option(x => x * -1L), arr1)) shouldBe "-3"
-//  }
+  it should "HigherOrderFunctions: arrayMax" in {
+    r(arrayMax[Long, Long](None, arr1)) shouldBe "3"
+    r(arrayMax[Long, Long](Option(x => x * -1L), arr1)) shouldBe "-1"
+  }
+
+  it should "HigherOrderFunctions: arrayMin" in {
+    r(arrayMin[Long, Long](None, arr1)) shouldBe "1"
+    r(arrayMin[Long, Long](Option(x => x * -1L), arr1)) shouldBe "-3"
+  }
 
   it should "HigherOrderFunctions: arrayReverseFill" in {
     r(arrayReverseFill[Long](_.isEq(2L), arr1)) shouldBe "[2,2,3]"
