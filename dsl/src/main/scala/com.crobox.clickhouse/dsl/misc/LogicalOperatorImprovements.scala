9ddcbee789f85fcd5dba6e045ed6088fcb76c932
Leonard Wolters <leonard@crobox.com>
2021-02-04 17:46:31 +0100

    Extended LogicalOperatorImprovements

--- dsl/src/main/scala/com.crobox.clickhouse/dsl/misc/LogicalOperatorImprovements.scala
+++ dsl/src/main/scala/com.crobox.clickhouse/dsl/misc/LogicalOperatorImprovements.scala
@@ -90,4 +90,67 @@ object LogicalOperatorImprovements {
       case (None, None)       => None
     }
   }
+
+  implicit class TableColumnImpr(left: TableColumn[Boolean]) {
+
+    def and(right: Option[TableColumn[Boolean]]): TableColumn[Boolean] = right match {
+      case Some(condition) => LogicalFunction(left, And, condition)
+      case _               => left
+    }
+
+    def and(right: TableColumn[Boolean]): TableColumn[Boolean] = LogicalFunction(left, And, right)
+
+    def or(right: Option[TableColumn[Boolean]]): TableColumn[Boolean] = right match {
+      case Some(condition) => LogicalFunction(left, Or, condition)
+      case _               => left
+    }
+
+    def or(right: TableColumn[Boolean]): TableColumn[Boolean] = LogicalFunction(left, Or, right)
+
+    def xor(right: Option[TableColumn[Boolean]]): TableColumn[Boolean] = right match {
+      case Some(condition) => LogicalFunction(left, Xor, condition)
+      case _               => left
+    }
+
+    def xor(right: TableColumn[Boolean]): TableColumn[Boolean] = LogicalFunction(left, Xor, right)
+  }
+
+  implicit class OptionalTableColumnImpr(left: Option[TableColumn[Boolean]]) {
+
+    def and(right: Option[TableColumn[Boolean]]): Option[TableColumn[Boolean]] = (left, right) match {
+      case (Some(l), Some(r)) => Option(LogicalFunction(l, And, r))
+      case (None, Some(r))    => Some(r)
+      case (Some(l), None)    => Some(l)
+      case (None, None)       => None
+    }
+
+    def and(right: TableColumn[Boolean]): TableColumn[Boolean] = left match {
+      case Some(l) => LogicalFunction(l, And, right)
+      case _       => right
+    }
+
+    def or(right: Option[TableColumn[Boolean]]): Option[TableColumn[Boolean]] = (left, right) match {
+      case (Some(l), Some(r)) => Option(LogicalFunction(l, Or, r))
+      case (None, Some(r))    => Some(r)
+      case (Some(l), None)    => Some(l)
+      case (None, None)       => None
+    }
+
+    def or(right: TableColumn[Boolean]): TableColumn[Boolean] = left match {
+      case Some(l) => LogicalFunction(l, Or, right)
+      case _       => right
+    }
+
+    def xor(right: Option[TableColumn[Boolean]]): Option[TableColumn[Boolean]] = (left, right) match {
+      case (Some(l), Some(r)) => Option(LogicalFunction(l, Xor, r))
+      case (None, Some(r))    => Some(r)
+      case (Some(l), None)    => Some(l)
+      case (None, None)       => None
+    }
+
+    def xor(right: TableColumn[Boolean]): TableColumn[Boolean] = left match {
+      case Some(l) => LogicalFunction(l, Xor, right)
+      case _       => right
+    }
+  }
 }
