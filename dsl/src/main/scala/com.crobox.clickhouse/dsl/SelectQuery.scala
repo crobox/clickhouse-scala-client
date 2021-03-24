a5f6f2ccb7d0c08355b6ab332b6c15826323a576
Leonard Wolters <leonard@crobox.com>
2021-03-24 17:28:42 +0100

    Added functionality to easily add/remove columns

--- dsl/src/main/scala/com.crobox.clickhouse/dsl/SelectQuery.scala
+++ dsl/src/main/scala/com.crobox.clickhouse/dsl/SelectQuery.scala
@@ -3,6 +3,9 @@ package com.crobox.clickhouse.dsl
 case class SelectQuery(columns: Seq[Column], modifier: String = "") extends Query with OperationalQuery {
   override val internalQuery = InternalQuery(Some(this))
 
+  def addColumn(column: Column): SelectQuery =
+    if (columns.exists(_.name == column.name)) copy(columns = columns ++ Seq(column)) else this
+
+  def removeColumn(column: Column): SelectQuery =
+    copy(columns = columns.filter(_.name != column.name))
 }
-
-
