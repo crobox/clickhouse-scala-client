602d0af0085317684f8dc4ea6742ce821411b6f4
Leonard Wolters <leonard@crobox.com>
2021-04-06 16:08:05 +0200

    Reverted akka http

--- project/Build.scala
+++ project/Build.scala
@@ -2,7 +2,8 @@ import sbt._
 object Build {
 
   val AkkaVersion     = "2.5.32"
-  val AkkaHttpVersion = "10.2.4"
+  val AkkaHttpVersion = "10.1.12"
+//  val AkkaHttpVersion = "10.2.4" # breaking changes with version 10.1
 
   val testDependencies = Seq("org.scalatest" %% "scalatest" % "3.2.6",
                              "com.typesafe.akka" %% "akka-testkit"   % AkkaVersion,
