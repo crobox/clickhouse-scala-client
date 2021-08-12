9324a0c9482b2f19b97cbee34643b30da89b5e4a
Leonard Wolters <leonard@crobox.com>
2021-08-12 13:05:13 +0200

    Updated all dependencies and akka to 2.6

--- project/Build.scala
+++ project/Build.scala
@@ -1,12 +1,13 @@
 import sbt._
+
 object Build {
 
-  val AkkaVersion     = "2.5.32"
-  val AkkaHttpVersion = "10.1.12"
-//  val AkkaHttpVersion = "10.2.4" # breaking changes with version 10.1
+  val AkkaVersion     = "2.6.15"
+  val AkkaHttpVersion = "10.2.6"
+//  val AkkaHttpVersion = "10.2.6" # breaking changes with version 10.1
 
-  val testDependencies = Seq("org.scalatest" %% "scalatest" % "3.2.6",
+  val testDependencies = Seq("org.scalatest" %% "scalatest" % "3.2.9",
                              "com.typesafe.akka" %% "akka-testkit"   % AkkaVersion,
-                             "ch.qos.logback"    % "logback-classic" % "1.2.3")
+                             "ch.qos.logback"    % "logback-classic" % "1.2.5")
 
 }
