8754eae554e69dcea2acd9cd8b0c3ab4a9781c02
Leonard Wolters <leonard@crobox.com>
2021-12-14 19:49:30 +0100

    Added executWithLogging feasibility method

--- dsl/src/main/scala/com.crobox.clickhouse/dsl/execution/ClickhouseQueryExecutor.scala
+++ dsl/src/main/scala/com.crobox.clickhouse/dsl/execution/ClickhouseQueryExecutor.scala
@@ -6,11 +6,13 @@ import com.crobox.clickhouse.dsl.language.{ClickhouseTokenizerModule, TokenizerM
 import com.crobox.clickhouse.dsl.{Query, Table}
 import com.crobox.clickhouse.internal.QuerySettings
 import com.crobox.clickhouse.internal.progress.QueryProgress.QueryProgress
+import com.typesafe.scalalogging.LazyLogging
 import spray.json.{JsonReader, _}
 
 import scala.concurrent.{ExecutionContext, Future}
 
-trait ClickhouseQueryExecutor extends QueryExecutor { self: TokenizerModule =>
+trait ClickhouseQueryExecutor extends QueryExecutor with LazyLogging {
+  self: TokenizerModule =>
   implicit val client: ClickhouseClient
 
   def execute[V: JsonReader](query: Query)(implicit executionContext: ExecutionContext,
@@ -18,6 +20,15 @@ trait ClickhouseQueryExecutor extends QueryExecutor { self: TokenizerModule =>
     import QueryResult._
     val queryResult = client.query(toSql(query.internalQuery))
     queryResult.map(_.parseJson.convertTo[QueryResult[V]])
+  }
+
+  def executeWithLogging[V: JsonReader](
+      query: Query
+  )(implicit executionContext: ExecutionContext, settings: QuerySettings = QuerySettings()): Future[QueryResult[V]] = {
+    import QueryResult._
+    val sql: String = toSql(query.internalQuery)
+    logger.info(s"SQL: $sql")
+    client.query(sql).map(_.parseJson.convertTo[QueryResult[V]])
   }
 
   def executeWithProgress[V: JsonReader](
