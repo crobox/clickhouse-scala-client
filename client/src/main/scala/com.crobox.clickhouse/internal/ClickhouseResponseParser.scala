349be8ae07d18f5785e91cc0c69a70becd1c5016
Leonard Wolters <leonard@crobox.com>
2022-01-04 11:15:13 +0100

    Maintenance

--- client/src/main/scala/com.crobox.clickhouse/internal/ClickhouseResponseParser.scala
+++ client/src/main/scala/com.crobox.clickhouse/internal/ClickhouseResponseParser.scala
@@ -1,12 +1,12 @@
 package com.crobox.clickhouse.internal
 
-import akka.http.scaladsl.coding.{Deflate, Gzip, NoCoding}
+import akka.http.scaladsl.coding.Coders
 import akka.http.scaladsl.model._
 import akka.http.scaladsl.model.headers.{HttpEncoding, HttpEncodings}
 import akka.http.scaladsl.unmarshalling.Unmarshaller
 import akka.stream.Materializer
 import akka.stream.scaladsl.SourceQueue
-import com.crobox.clickhouse.internal.progress.QueryProgress.{QueryProgress, _}
+import com.crobox.clickhouse.internal.progress.QueryProgress._
 import com.crobox.clickhouse.{ClickhouseChunkedException, ClickhouseException}
 
 import scala.concurrent.{ExecutionContext, Future}
@@ -24,42 +24,46 @@ private[clickhouse] trait ClickhouseResponseParser {
     responseFuture.flatMap { response =>
       decodeResponse(response) match {
         case HttpResponse(StatusCodes.OK, _, entity, _) =>
-          Unmarshaller.stringUnmarshaller(entity).map(content => {
-            if (content.contains("DB::Exception")) { //FIXME this is quite a fragile way to detect failures, hopefully nobody will have a valid exception string in the result. Check https://github.com/yandex/ClickHouse/issues/2999
-              throw ClickhouseException("Found exception in the query return body",
-                                        query,
-                                        ClickhouseChunkedException(content),
-                                        StatusCodes.OK)
+          Unmarshaller
+            .stringUnmarshaller(entity)
+            .map(content => {
+              if (content.contains("DB::Exception")) { //FIXME this is quite a fragile way to detect failures, hopefully nobody will have a valid exception string in the result. Check https://github.com/yandex/ClickHouse/issues/2999
+                throw ClickhouseException("Found exception in the query return body",
+                                          query,
+                                          ClickhouseChunkedException(content),
+                                          StatusCodes.OK)
+              }
+              content
+            })
+            .andThen {
+              case Success(_) =>
+                progressQueue.foreach(queue => {
+                  queue.offer(QueryFinished)
+                })
+              case Failure(exception) =>
+                progressQueue.foreach(queue => {
+                  queue.offer(QueryFailed(exception))
+                })
             }
-            content
-          })
-          .andThen {
-            case Success(_) =>
-              progressQueue.foreach(queue => {
-                queue.offer(QueryFinished)
-              })
-            case Failure(exception) =>
-              progressQueue.foreach(queue => {
-                queue.offer(QueryFailed(exception))
-              })
-          }
         case HttpResponse(code, _, entity, _) =>
           progressQueue.foreach(_.offer(QueryRejected))
-          Unmarshaller.stringUnmarshaller(entity).flatMap(
-            response =>
-              Future.failed(
+          Unmarshaller
+            .stringUnmarshaller(entity)
+            .flatMap(
+              response =>
+                Future.failed(
                   ClickhouseException(s"Server [$host] returned code $code; $response", query, statusCode = code)
               )
-          )
+            )
       }
     }
 
   protected def decodeResponse(response: HttpResponse): HttpResponse = {
     val decoder = response.encoding match {
-      case HttpEncodings.gzip => Gzip
-      case HttpEncodings.deflate => Deflate
-      case HttpEncodings.identity => NoCoding
-      case HttpEncoding(enc) => throw new IllegalArgumentException(s"Unsupported response encoding: $enc")
+      case HttpEncodings.gzip     => Coders.Gzip
+      case HttpEncodings.deflate  => Coders.Deflate
+      case HttpEncodings.identity => Coders.NoCoding
+      case HttpEncoding(enc)      => throw new IllegalArgumentException(s"Unsupported response encoding: $enc")
     }
     decoder.decodeMessage(response)
   }
