80826943e6993bc8d9e3eb4a1a86e851ba6fa46b
Leonard Wolters <leonard@crobox.com>
2022-01-05 16:50:28 +0100

    Added tests for in/global/not/glboal not

--- dsl/src/main/scala/com.crobox.clickhouse/dsl/language/InFunctionTokenizer.scala
+++ dsl/src/main/scala/com.crobox.clickhouse/dsl/language/InFunctionTokenizer.scala
@@ -14,21 +14,22 @@ trait InFunctionTokenizer {
 
   private def tokenizeInFunctionCol(col: InFunctionCol[_])(implicit ctx: TokenizeContext): String = col match {
     case In(l: ConstOrColMagnet[_], r: InFuncRHMagnet) =>
-      s"${tokenizeColumn(l.column)} IN ${tokenizeInFunRHCol(r)}"
+      s"${tokenizeColumn(l.column)} IN ${tokenizeInFunRHCol(r, true)}"
     case NotIn(l: ConstOrColMagnet[_], r: InFuncRHMagnet) =>
-      s"${tokenizeColumn(l.column)} NOT IN ${tokenizeInFunRHCol(r)}"
+      s"${tokenizeColumn(l.column)} NOT IN ${tokenizeInFunRHCol(r, true)}"
     case GlobalIn(l: ConstOrColMagnet[_], r: InFuncRHMagnet) =>
-      s"${tokenizeColumn(l.column)} GLOBAL IN ${tokenizeInFunRHCol(r)}"
+      s"${tokenizeColumn(l.column)} GLOBAL IN ${tokenizeInFunRHCol(r, false)}"
     case GlobalNotIn(l: ConstOrColMagnet[_], r: InFuncRHMagnet) =>
-      s"${tokenizeColumn(l.column)} GLOBAL NOT IN ${tokenizeInFunRHCol(r)}"
+      s"${tokenizeColumn(l.column)} GLOBAL NOT IN ${tokenizeInFunRHCol(r, false)}"
   }
 
-  private def tokenizeInFunRHCol(value: InFuncRHMagnet)(implicit ctx: TokenizeContext): String = value match {
-    case col: InFuncRHMagnet if col.query.isDefined =>
-      // TODO: should we create a new context and simple 'reuse' the current?
-      s"(${toRawSql(col.query.get.internalQuery)(ctx.copy(useTableAlias = true))})"
-    case col: InFuncRHMagnet if col.tableRef.isDefined =>
-      col.tableRef.map(table => table.quoted + ctx.tableAlias(table)).get
-    case col: InFuncRHMagnet => tokenizeColumn(col.column)
-  }
+  private def tokenizeInFunRHCol(value: InFuncRHMagnet, useTableAlias: Boolean)(implicit ctx: TokenizeContext): String =
+    value match {
+      case col: InFuncRHMagnet if col.query.isDefined =>
+        // TODO: should we create a new context and simple 'reuse' the current?
+        s"(${toRawSql(col.query.get.internalQuery)(ctx.copy(useTableAlias = useTableAlias))})"
+      case col: InFuncRHMagnet if col.tableRef.isDefined =>
+        col.tableRef.map(table => table.quoted + ctx.tableAlias(table)).get
+      case col: InFuncRHMagnet => tokenizeColumn(col.column)
+    }
 }
---
+++ dsl/src/test/scala/com/crobox/clickhouse/dsl/column/INFunctionsTest.scala
@@ -0,0 +1,36 @@
+package com.crobox.clickhouse.dsl.column
+
+import com.crobox.clickhouse.ClickhouseSQLSupport
+import com.crobox.clickhouse.dsl._
+import com.crobox.clickhouse.dsl.language.ClickhouseTokenizerModule
+
+class INFunctionsTest extends ColumnFunctionTest with ClickhouseTokenizerModule with ClickhouseSQLSupport {
+
+  it should "use tableAlias for IN" in {
+    toSQL(shieldId.in(select(itemId).from(OneTestTable).where(itemId.===("a")))) should matchSQL(
+      s"""
+        |shield_id IN (SELECT item_id FROM ${OneTestTable.quoted} AS T1 WHERE item_id = 'a')
+        |""".stripMargin)
+  }
+
+  it should "use tableAlias for NOT IN" in {
+    toSQL(shieldId.notIn(select(itemId).from(OneTestTable).where(itemId.===("a")))) should matchSQL(
+      s"""
+         |shield_id NOT IN (SELECT item_id FROM ${OneTestTable.quoted} AS T1 WHERE item_id = 'a')
+         |""".stripMargin)
+  }
+
+  it should "SKIP tableAlias for GLOBAL IN" in {
+    toSQL(shieldId.globalIn(select(itemId).from(OneTestTable).where(itemId.===("a")))) should matchSQL(
+      s"""
+         |shield_id GLOBAL IN (SELECT item_id FROM ${OneTestTable.quoted} WHERE item_id = 'a')
+         |""".stripMargin)
+  }
+
+  it should "SKIP tableAlias for GLOBAL NOT IN" in {
+    toSQL(shieldId.globalNotIn(select(itemId).from(OneTestTable).where(itemId.===("a")))) should matchSQL(
+      s"""
+         |shield_id GLOBAL NOT IN (SELECT item_id FROM ${OneTestTable.quoted} WHERE item_id = 'a')
+         |""".stripMargin)
+  }
+}
