1bbd30e6e3ce0b296e052db99223c09068151f64
Leonard Wolters <leonard@crobox.com>
2022-01-05 15:59:23 +0100

    Small improvements

--- dsl/src/main/scala/com.crobox.clickhouse/dsl/column/InFunctions.scala
+++ dsl/src/main/scala/com.crobox.clickhouse/dsl/column/InFunctions.scala
@@ -39,13 +39,19 @@ trait InFunctions { self: Magnets =>
     if (r.isEmptyCollection) Const(false) else In(l, r)
 
   def notIn(l: ConstOrColMagnet[_], r: InFuncRHMagnet): ExpressionColumn[Boolean] =
-    if (r.isEmptyCollection) Const(false) else NotIn(l, r)
+    if (r.isEmptyCollection) Const(true) else NotIn(l, r)
 
   def globalIn(l: ConstOrColMagnet[_], r: InFuncRHMagnet): ExpressionColumn[Boolean] =
     if (r.isEmptyCollection) Const(false) else GlobalIn(l, r)
 
   def globalNotIn(l: ConstOrColMagnet[_], r: InFuncRHMagnet): ExpressionColumn[Boolean] =
-    if (r.isEmptyCollection) Const(false) else GlobalNotIn(l, r)
+    if (r.isEmptyCollection) Const(true) else GlobalNotIn(l, r)
+
+  def in(l: ConstOrColMagnet[_], r: InFuncRHMagnet, global: Boolean): ExpressionColumn[Boolean] =
+    if (global) globalIn(l, r) else in(l, r)
+
+  def notIn(l: ConstOrColMagnet[_], r: InFuncRHMagnet, global: Boolean): ExpressionColumn[Boolean] =
+    if (global) globalNotIn(l, r) else notIn(l, r)
 
   def tuple(coln: ConstOrColMagnet[_]*): Tuple                             = Tuple(coln)
   def tupleElement[T](tuple: Tuple, index: NumericCol[_]): TupleElement[T] = TupleElement[T](tuple, index)
