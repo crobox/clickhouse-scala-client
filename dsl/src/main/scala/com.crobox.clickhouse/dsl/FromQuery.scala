a8482dbe0c970371855e8addd26c772497271115
Leonard Wolters <leonard@crobox.com>
2022-01-25 14:44:02 +0100

    Made innerQuery not private

--- dsl/src/main/scala/com.crobox.clickhouse/dsl/FromQuery.scala
+++ dsl/src/main/scala/com.crobox.clickhouse/dsl/FromQuery.scala
@@ -1,13 +1,12 @@
 package com.crobox.clickhouse.dsl
 
 sealed trait FromQuery extends Query with OperationalQuery {
-  override val internalQuery = InternalQuery(from = Some(this))
+  override val internalQuery: InternalQuery = InternalQuery(from = Some(this))
   val alias: Option[String]
   val finalized: Boolean
 }
 
-sealed case class InnerFromQuery(private[dsl] val innerQuery: OperationalQuery, alias: Option[String] = None)
-    extends FromQuery {
+sealed case class InnerFromQuery(innerQuery: OperationalQuery, alias: Option[String] = None) extends FromQuery {
 
   /** Queries can never have 'final' clause: Illegal FINAL */
   override val finalized = false
