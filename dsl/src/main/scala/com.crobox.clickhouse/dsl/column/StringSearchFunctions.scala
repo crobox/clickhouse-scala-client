91658682fd6f721386b7675f491965682dc24471
Leonard Wolters <lwolters@gmail.com>
2022-02-01 15:43:17 +0100

    Feature/ilike (#131)
    * Added iLike (case insensitive)
    
    * Added support for ILike
    
    * Enabled tests for version 21 only
    
    * Small additions
    
    * Added case insensitive position
    
    * Added case insensitive position
    
    * Added case insensitive position
    
    Co-authored-by: Leonard Wolters <leonard@crobox.com>

--- dsl/src/main/scala/com.crobox.clickhouse/dsl/column/StringSearchFunctions.scala
+++ dsl/src/main/scala/com.crobox.clickhouse/dsl/column/StringSearchFunctions.scala
@@ -20,7 +20,8 @@ trait StringSearchFunctions { self: Magnets =>
       extends StringSearchFunc[String](col, pattern)
   case class ExtractAll(col: StringColMagnet[_], pattern: StringColMagnet[_])
       extends StringSearchFunc[Iterable[String]](col, pattern)
-  case class Like(col: StringColMagnet[_], pattern: StringColMagnet[_]) extends StringSearchFunc[Boolean](col, pattern)
+  case class ILike(col: StringColMagnet[_], pattern: StringColMagnet[_]) extends StringSearchFunc[Boolean](col, pattern)
+  case class Like(col: StringColMagnet[_], pattern: StringColMagnet[_])  extends StringSearchFunc[Boolean](col, pattern)
   case class NotLike(col: StringColMagnet[_], pattern: StringColMagnet[_])
       extends StringSearchFunc[Boolean](col, pattern)
   case class ReplaceOne(col: StringColMagnet[_], pattern: StringColMagnet[_], replacement: StringColMagnet[_])
@@ -32,10 +33,18 @@ trait StringSearchFunctions { self: Magnets =>
   case class ReplaceRegexpAll(col: StringColMagnet[_], pattern: StringColMagnet[_], replacement: StringColMagnet[_])
       extends StringSearchReplaceFunc(col, pattern, replacement)
 
-  def position(col: StringColMagnet[_], needle: StringColMagnet[_]): Position = Position(col, needle)
+  def position(col: StringColMagnet[_], needle: StringColMagnet[_]): Position =
+    Position(col, needle, caseSensitive = true)
+
+  def position(col: StringColMagnet[_], needle: StringColMagnet[_], caseInsensitive: Boolean): Position =
+    if (caseInsensitive) positionCaseInsensitive(col, needle) else position(col, needle)
 
   def positionCaseInsensitive(col: StringColMagnet[_], needle: StringColMagnet[_]): Position =
     Position(col, needle, caseSensitive = false)
+
+  def positionUTF8(col: StringColMagnet[_], needle: StringColMagnet[_], caseInsensitive: Boolean): PositionUTF8 =
+    if (caseInsensitive) positionUTF8CaseInsensitive(col, needle) else positionUTF8(col, needle)
+
   def positionUTF8(col: StringColMagnet[_], needle: StringColMagnet[_]): PositionUTF8 = PositionUTF8(col, needle)
 
   def positionUTF8CaseInsensitive(col: StringColMagnet[_], needle: StringColMagnet[_]): PositionUTF8 =
@@ -43,8 +52,12 @@ trait StringSearchFunctions { self: Magnets =>
   def strMatch(col: StringColMagnet[_], pattern: StringColMagnet[_]): StrMatch     = StrMatch(col, pattern)
   def extract(col: StringColMagnet[_], pattern: StringColMagnet[_]): Extract       = Extract(col, pattern)
   def extractAll(col: StringColMagnet[_], pattern: StringColMagnet[_]): ExtractAll = ExtractAll(col, pattern)
+  def iLike(col: StringColMagnet[_], pattern: StringColMagnet[_]): ILike           = ILike(col, pattern)
   def like(col: StringColMagnet[_], pattern: StringColMagnet[_]): Like             = Like(col, pattern)
-  def notLike(col: StringColMagnet[_], pattern: StringColMagnet[_]): NotLike       = NotLike(col, pattern)
+
+  def like(col: StringColMagnet[_], pattern: StringColMagnet[_], caseInsensitive: Boolean): StringSearchFunc[Boolean] =
+    if (caseInsensitive) iLike(col, pattern) else like(col, pattern)
+  def notLike(col: StringColMagnet[_], pattern: StringColMagnet[_]): NotLike = NotLike(col, pattern)
 
   def replaceOne(col: StringColMagnet[_], pattern: StringColMagnet[_], replacement: StringColMagnet[_]): ReplaceOne =
     ReplaceOne(col, pattern, replacement)
@@ -61,15 +74,25 @@ trait StringSearchFunctions { self: Magnets =>
                        replacement: StringColMagnet[_]): ReplaceRegexpAll = ReplaceRegexpAll(col, pattern, replacement)
 
   trait StringSearchOps { self: StringColMagnet[_] =>
-    def position(needle: StringColMagnet[_]): Position                        = Position(self, needle)
-    def positionCaseInsensitive(needle: StringColMagnet[_]): Position         = Position(self, needle, false)
-    def positionUTF8(needle: StringColMagnet[_]): PositionUTF8                = PositionUTF8(self, needle)
+
+    def position(needle: StringColMagnet[_], caseInsensitive: Boolean): Position =
+      if (caseInsensitive) positionCaseInsensitive(needle) else position(needle)
+    def position(needle: StringColMagnet[_]): Position                = Position(self, needle, true)
+    def positionCaseInsensitive(needle: StringColMagnet[_]): Position = Position(self, needle, false)
+
+    def positionUTF8(needle: StringColMagnet[_], caseInsensitive: Boolean): PositionUTF8 =
+      if (caseInsensitive) positionUTF8CaseInsensitive(needle) else positionUTF8(needle)
+    def positionUTF8(needle: StringColMagnet[_]): PositionUTF8                = PositionUTF8(self, needle, true)
     def positionUTF8CaseInsensitive(needle: StringColMagnet[_]): PositionUTF8 = PositionUTF8(self, needle, false)
     def strMatch(pattern: StringColMagnet[_]): StrMatch                       = StrMatch(self, pattern)
     def extract(pattern: StringColMagnet[_]): Extract                         = Extract(self, pattern)
     def extractAll(pattern: StringColMagnet[_]): ExtractAll                   = ExtractAll(self, pattern)
+    def iLike(pattern: StringColMagnet[_]): ILike                             = ILike(self, pattern)
     def like(pattern: StringColMagnet[_]): Like                               = Like(self, pattern)
-    def notLike(pattern: StringColMagnet[_]): NotLike                         = NotLike(self, pattern)
+
+    def like(pattern: StringColMagnet[_], caseInsensitive: Boolean): StringSearchFunc[Boolean] =
+      if (caseInsensitive) ILike(self, pattern) else Like(self, pattern)
+    def notLike(pattern: StringColMagnet[_]): NotLike = NotLike(self, pattern)
 
     def replaceOne(pattern: StringColMagnet[_], replacement: StringColMagnet[_]): ReplaceOne =
       ReplaceOne(self, pattern, replacement)
