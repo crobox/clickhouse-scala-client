afa44a91ae2d7f7dde66471d568d5d1af93457ac
Leonard Wolters <leonard@crobox.com>
2022-03-03 14:54:52 +0100

    Added tests

--- dsl/src/test/scala/com/crobox/clickhouse/dsl/column/UUIDFunctionsTest.scala
+++ dsl/src/test/scala/com/crobox/clickhouse/dsl/column/UUIDFunctionsTest.scala
@@ -6,21 +6,39 @@ import com.crobox.clickhouse.dsl._
 class UUIDFunctionsTest extends DslTestSpec {
 
   it should "rewrite empty to empty(0)" in {
-    var query = select(All()).from(TwoTestTable).where(nativeUUID.empty())
-    toSql(query.internalQuery, None) should matchSQL(
-      s"SELECT * FROM $database.twoTestTable WHERE uuid == 0"
-    )
+    val query = select(All()).from(TwoTestTable).where(nativeUUID.empty())
+    if (ClickHouseVersion.minimalVersion(21, 8)) {
+      toSql(query.internalQuery, None) should matchSQL(
+        s"SELECT * FROM $database.twoTestTable WHERE empty(uuid)"
+      )
+    } else {
+      toSql(query.internalQuery, None) should matchSQL(
+        s"SELECT * FROM $database.twoTestTable WHERE uuid == 0"
+      )
+    }
   }
 
   it should "rewrite notEmpty to notEquals(0)" in {
     var query = select(All()).from(TwoTestTable).where(nativeUUID.notEmpty())
-    toSql(query.internalQuery, None) should matchSQL(
-      s"SELECT * FROM $database.twoTestTable WHERE uuid != 0"
-    )
+    if (ClickHouseVersion.minimalVersion(21, 8)) {
+      toSql(query.internalQuery, None) should matchSQL(
+        s"SELECT * FROM $database.twoTestTable WHERE notEmpty(uuid)"
+      )
+    } else {
+      toSql(query.internalQuery, None) should matchSQL(
+        s"SELECT * FROM $database.twoTestTable WHERE uuid != 0"
+      )
+    }
 
     query = select(All()).from(TwoTestTable).where(notEmpty(nativeUUID))
-    toSql(query.internalQuery, None) should matchSQL(
-      s"SELECT * FROM $database.twoTestTable WHERE uuid != 0"
-    )
+    if (ClickHouseVersion.minimalVersion(21, 8)) {
+      toSql(query.internalQuery, None) should matchSQL(
+        s"SELECT * FROM $database.twoTestTable WHERE notEmpty(uuid)"
+      )
+    } else {
+      toSql(query.internalQuery, None) should matchSQL(
+        s"SELECT * FROM $database.twoTestTable WHERE uuid != 0"
+      )
+    }
   }
 }
