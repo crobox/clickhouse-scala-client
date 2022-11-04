1feb71a9b70e8c883c32fa6ef3d4bb83e8d094d7
Majd Baladi <majd@crobox.com>
2022-11-04 14:40:55 +0100

    fixes and code optimization.

--- client/src/main/scala/com.crobox.clickhouse/stream/ClickhouseSink.scala
+++ client/src/main/scala/com.crobox.clickhouse/stream/ClickhouseSink.scala
@@ -23,44 +23,11 @@ case class Optimize(table: String, cluster: Option[String]) extends TableOperati
 
 object ClickhouseSink extends LazyLogging {
 
-  /**
-   * @deprecated added support to optimize table operations <br/>
-   *             use [[#toSink()]] instead.
-   */
-  @deprecated
+  @deprecated("use [[#toSink()]] instead")
   def insertSink(config: Config, client: ClickhouseClient, indexerName: Option[String] = None)(
       implicit ec: ExecutionContext,
       settings: QuerySettings = QuerySettings()
-  ): Sink[Insert, Future[Done]] = {
-    val indexerGeneralConfig = config.getConfig("crobox.clickhouse.indexer")
-    val mergedIndexerConfig = indexerName
-      .flatMap(
-        theIndexName =>
-          if (indexerGeneralConfig.hasPath(theIndexName))
-            Some(indexerGeneralConfig.getConfig(theIndexName).withFallback(indexerGeneralConfig))
-          else None
-      )
-      .getOrElse(indexerGeneralConfig)
-    val batchSize     = mergedIndexerConfig.getInt("batch-size")
-    val flushInterval = mergedIndexerConfig.getDuration("flush-interval").getSeconds.seconds
-    Flow[Insert]
-      .groupBy(Int.MaxValue, _.table)
-      .groupedWithin(batchSize, flushInterval)
-      .mapAsyncUnordered(mergedIndexerConfig.getInt("concurrent-requests"))(inserts => {
-        val table       = inserts.head.table
-        val insertQuery = s"INSERT INTO $table FORMAT JSONEachRow"
-        val payload     = inserts.map(_.jsonRow)
-        logger.debug(s"Inserting ${inserts.size} entries in table: $table. Group Within: ($batchSize - $flushInterval)")
-        client
-          .execute(insertQuery, payload.mkString("\n"))
-          .recover {
-            case ex => throw ClickhouseIndexingException("failed to index", ex, payload, table)
-          }
-          .map(_ => inserts)
-      })
-      .mergeSubstreams
-      .toMat(Sink.ignore)(Keep.right)
-  }
+  ): Sink[Insert, Future[Done]] = toSink(config, client, indexerName)
 
   def toSink(config: Config, client: ClickhouseClient, indexerName: Option[String] = None)(
       implicit ec: ExecutionContext,
@@ -99,11 +66,10 @@ object ClickhouseSink extends LazyLogging {
       settings: QuerySettings = QuerySettings()
   ): Future[Iterable[String]] =
     Future.sequence(ops.groupBy(_.table).map { t =>
-      val insertQuery = s"INSERT INTO ${t._1} FORMAT JSONEachRow"
-      val payload     = t._2.map(_.jsonRow)
-      logger.info(s"Inserting ${payload.size} entries in table: ${t._1}.")
+      val payload = t._2.map(_.jsonRow)
+      logger.debug(s"Inserting ${payload.size} entries in table: ${t._1}.")
       client
-        .execute(insertQuery, payload.mkString("\n"))
+        .execute(s"INSERT INTO ${t._1} FORMAT JSONEachRow", payload.mkString("\n"))
         .recover {
           case ex => throw ClickhouseIndexingException("failed to index", ex, payload, t._1)
         }
@@ -113,17 +79,15 @@ object ClickhouseSink extends LazyLogging {
       implicit ec: ExecutionContext,
       settings: QuerySettings = QuerySettings()
   ): Future[Iterable[String]] = {
-    logger.info(s"Optimizing tables")
+    logger.debug(s"Optimizing tables")
     Future.sequence(
       ops.map { o =>
-        val query = buildOptimizeQuery(o.table, o.cluster)
-        client.execute(query).recover {
-          case ex => throw ClickhouseIndexingException(s"failed to optimize ${o.table}", ex, Seq(), o.table)
-        }
+        client
+          .execute(s"OPTIMIZE TABLE ${o.table}${o.cluster.map(s => s"_local ON CLUSTER $s").getOrElse("")} FINAL")
+          .recover {
+            case ex => throw ClickhouseIndexingException(s"failed to optimize ${o.table}", ex, Seq(), o.table)
+          }
       }
     )
   }
-
-  private def buildOptimizeQuery(table: String, cluster: Option[String]): String =
-    s"OPTIMIZE TABLE $table${cluster.map(s => s"_local ON CLUSTER $s").getOrElse("")} FINAL"
 }
