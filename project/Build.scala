9d7ab535f7cd1f0a0bcf62dea452135e8d9f4675
Leonard Wolters <leonard@crobox.com>
2023-04-21 11:18:09 +0200

    Upgraded dependencies

--- project/Build.scala
+++ project/Build.scala
@@ -2,10 +2,10 @@ import sbt._
 
 object Build {
 
-  val AkkaVersion     = "2.6.19"
-  val AkkaHttpVersion = "10.2.8"
+  val AkkaVersion     = "2.6.20"  // do *not* upgrade to 2.7+ (commercial license)
+  val AkkaHttpVersion = "10.2.10" // do *not* upgrade to 10.4+ (commercial license)
 
-  val testDependencies = Seq("org.scalatest" %% "scalatest" % "3.2.12",
+  val testDependencies = Seq("org.scalatest" %% "scalatest" % "3.2.15",
                              "com.typesafe.akka" %% "akka-testkit"   % AkkaVersion,
-                             "ch.qos.logback"    % "logback-classic" % "1.2.11")
+                             "ch.qos.logback"    % "logback-classic" % "1.4.6")
 }
