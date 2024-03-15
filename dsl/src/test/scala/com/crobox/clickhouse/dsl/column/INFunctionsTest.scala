48fc216e3fede431e2ac8a30cc6b9e5117942054
Andriy Shevchenko <andriyshevchenko@vandebron.nl>
2024-03-15 01:58:58 +0100

    Test fix

--- dsl/src/test/scala/com/crobox/clickhouse/dsl/column/INFunctionsTest.scala
+++ dsl/src/test/scala/com/crobox/clickhouse/dsl/column/INFunctionsTest.scala
@@ -3,75 +3,70 @@ package com.crobox.clickhouse.dsl.column
 import com.crobox.clickhouse.DslTestSpec
 import com.crobox.clickhouse.dsl.JoinQuery.InnerJoin
 import com.crobox.clickhouse.dsl._
-import com.crobox.clickhouse.dsl.column.ClickhouseColumnFunctions.{constOrColMagnetFromCol => toCol}
 import scala.language.implicitConversions
 
 
 class INFunctionsTest extends DslTestSpec {
 
+  private val shieldCol: ConstOrColMagnet[String] = shieldId
+  private val itemCol: ConstOrColMagnet[String] = itemId
+
   it should "use tableAlias for IN" in {
-    //val shieldToUse: ConstOrColMagnet[String] =
-    val resToSee = toSQL(toCol(shieldId).in(select(itemId).from(OneTestTable).where(itemId.isEq("a"))))
-
-    println("==================================")
-    println(s"in SQL building result: $resToSee")
-    println("==================================")
-
-    resToSee should matchSQL(
+    toSQL(shieldCol.in(select(itemId).from(OneTestTable).where(itemId.isEq("a")))) should matchSQL(
       s"""
          |shield_id IN (SELECT item_id FROM ${OneTestTable.quoted} AS T1 WHERE item_id = 'a')
          |""".stripMargin)
   }
 
-  //  it should "use tableAlias for NOT IN" in {
-  //    toSQL(shieldId.notIn(select(itemId).from(OneTestTable).where(itemId.isEq("a")
-  //    ))) should matchSQL(
-  //      s"""
-  //         |shield_id NOT IN (SELECT item_id FROM ${OneTestTable.quoted} AS T1 WHERE item_id = 'a')
-  //         |""".stripMargin)
-  //  }
-  //
-  //  it should "SKIP tableAlias for IN as select clause" in {
-  //    toSQL(select(itemId).from(OneTestTable).where(itemId.in(Seq("a", "b"))), false) should matchSQL(
-  //      s"""
-  //         |SELECT item_id FROM ${OneTestTable.quoted} WHERE item_id IN ('a', 'b')
-  //         |""".stripMargin)
-  //
-  //    toSQL(select(itemId.in(Seq("a", "b")) as "l").from(OneTestTable)
-  //      .where(itemId.isEq("a")), false) should matchSQL(
-  //      s"""
-  //         |SELECT item_id IN ('a', 'b') AS l FROM ${OneTestTable.quoted} WHERE item_id = 'a'
-  //         |""".stripMargin)
-  //  }
-  //
-  //  it should "SKIP tableAlias for GLOBAL IN" in {
-  //    toSQL(shieldId.globalIn(select(itemId).from(OneTestTable).where(itemId.isEq("a")
-  //    ))) should matchSQL(
-  //      s"""
-  //         |shield_id GLOBAL IN (SELECT item_id FROM ${OneTestTable.quoted} WHERE item_id = 'a')
-  //         |""".stripMargin)
-  //  }
-  //
-  //  it should "SKIP tableAlias for GLOBAL NOT IN" in {
-  //    toSQL(shieldId.globalNotIn(select(itemId).from(OneTestTable).where(itemId.isEq("a")
-  //    ))) should matchSQL(
-  //      s"""
-  //         |shield_id GLOBAL NOT IN (SELECT item_id FROM ${OneTestTable.quoted} WHERE item_id = 'a')
-  //         |""".stripMargin)
-  //  }
-  //
-  //  it should "use tableAlias for nested IN" in {
-  //    toSQL(
-  //      shieldId.in(select(itemId).from(OneTestTable).join(InnerJoin, select(itemId).from(TwoTestTable)).using(itemId))
-  //    ) should matchSQL(
-  //      s"""
-  //         |shield_id IN (
-  //         |    SELECT item_id FROM ${OneTestTable.quoted} AS L1
-  //         |    INNER JOIN (SELECT item_id FROM ${TwoTestTable.quoted}) AS R1 USING item_id)
-  //         |""".stripMargin
-  //    )
-  //  }
-  //
+  it should "use tableAlias for NOT IN" in {
+    toSQL(shieldCol.notIn(select(itemId).from(OneTestTable).where(itemId.isEq("a")
+    ))) should matchSQL(
+      s"""
+         |shield_id NOT IN (SELECT item_id FROM ${OneTestTable.quoted} AS T1 WHERE item_id = 'a')
+         |""".stripMargin)
+  }
+
+  it should "SKIP tableAlias for IN as select clause" in {
+    toSQL(select(itemId).from(OneTestTable).where(itemCol.in(Seq("a", "b"))), false) should matchSQL(
+      s"""
+         |SELECT item_id FROM ${OneTestTable.quoted} WHERE item_id IN ('a', 'b')
+         |""".stripMargin)
+
+    toSQL(select(itemCol.in(Seq("a", "b")) as "l").from(OneTestTable)
+      .where(itemId.isEq("a")), false) should matchSQL(
+      s"""
+         |SELECT item_id IN ('a', 'b') AS l FROM ${OneTestTable.quoted} WHERE item_id = 'a'
+         |""".stripMargin)
+  }
+
+  it should "SKIP tableAlias for GLOBAL IN" in {
+    toSQL(shieldCol.globalIn(select(itemId).from(OneTestTable).where(itemId.isEq("a")
+    ))) should matchSQL(
+      s"""
+         |shield_id GLOBAL IN (SELECT item_id FROM ${OneTestTable.quoted} WHERE item_id = 'a')
+         |""".stripMargin)
+  }
+
+  it should "SKIP tableAlias for GLOBAL NOT IN" in {
+    toSQL(shieldCol.globalNotIn(select(itemId).from(OneTestTable).where(itemId.isEq("a")
+    ))) should matchSQL(
+      s"""
+         |shield_id GLOBAL NOT IN (SELECT item_id FROM ${OneTestTable.quoted} WHERE item_id = 'a')
+         |""".stripMargin)
+  }
+
+  it should "use tableAlias for nested IN" in {
+    toSQL(
+      shieldCol.in(select(itemId).from(OneTestTable).join(InnerJoin, select(itemId).from(TwoTestTable)).using(itemId))
+    ) should matchSQL(
+      s"""
+         |shield_id IN (
+         |    SELECT item_id FROM ${OneTestTable.quoted} AS L1
+         |    INNER JOIN (SELECT item_id FROM ${TwoTestTable.quoted}) AS R1 USING item_id)
+         |""".stripMargin
+    )
+  }
+
   //  it should "use tableAlias for IN multiple tables" in {
   //    toSQL(
   //      (
@@ -89,6 +84,6 @@ class INFunctionsTest extends DslTestSpec {
   //         |  AND column_2 IN (SELECT column_2 FROM ${TwoTestTable.quoted} AS T2)
   //         |  AND column_2 IN (SELECT column_4 FROM ${ThreeTestTable.quoted} AS T1)
   //         |""".stripMargin
-  //    )
-  //  }
+  )
+}
 }
