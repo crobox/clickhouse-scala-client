ace9a613a9baf7048f4e8691edcfa59aa9feeea1
Leonard Wolters <leonard@crobox.com>
2024-05-29 12:57:39 +0200

    Simplyfying delimiters

--- dsl/src/main/scala/com/crobox/clickhouse/dsl/language/SplitMergeFunctionTokenizer.scala
+++ dsl/src/main/scala/com/crobox/clickhouse/dsl/language/SplitMergeFunctionTokenizer.scala
@@ -7,27 +7,31 @@ trait SplitMergeFunctionTokenizer {
 
   def tokenizeSplitMergeFunction(col: SplitMergeFunction[_])(implicit ctx: TokenizeContext): String = col match {
     case SplitByChar(sep: StringColMagnet[_], col: StringColMagnet[_]) =>
-      sep.column match {
-        case c: Const[String] =>
-          val separator = c.const
-          if (separator.length == 1) {
-            val s = separator.charAt(0).toInt
+      // Some small optimizations
+      val separator = sep.column match {
+        case c: Const[_] => c.const.asInstanceOf[String]
+        case _ =>
+          var s = tokenizeColumn(sep.column)
+          if (s.startsWith("`")) s = s.tail
+          if (s.endsWith("`")) s = s.init
+          s
+      }
+      if (separator.length == 1) {
+        val s = separator.charAt(0).toInt
 
-            // https://en.wikipedia.org/wiki/List_of_Unicode_characters
-            // 34 == DoubleQuote ("), 39 == Single Quote ('),
-            // 47 == Forward Slash (/), 92 == Backward Slash (\\)
-            // 96 == Grave Accent (` under tilde)
-            if (s == 34 || s == 39 || s == 92 || s == 96) {
-              s"splitByChar(char($s), ${tokenizeColumn(col.column)})"
-            } else if (s >= 32 && s <= 126) {
-              s"splitByChar(${tokenizeColumn(sep.column)}, ${tokenizeColumn(col.column)})"
-            } else {
-              s"splitByChar(char($s), ${tokenizeColumn(col.column)})"
-            }
-          } else {
-            s"splitByString(${tokenizeColumn(sep.column)}, ${tokenizeColumn(col.column)})"
-          }
-        case _ => s"splitByString(${tokenizeColumn(sep.column)}, ${tokenizeColumn(col.column)})"
+        // https://en.wikipedia.org/wiki/List_of_Unicode_characters
+        // 34 == DoubleQuote ("), 39 == Single Quote ('),
+        // 47 == Forward Slash (/), 92 == Backward Slash (\\)
+        // 96 == Grave Accent (` under tilde)
+        if (s == 34 || s == 39 || s == 92 || s == 96) {
+          s"splitByChar(char($s), ${tokenizeColumn(col.column)})"
+        } else if (s >= 32 && s <= 126) {
+          s"splitByChar(${tokenizeColumn(sep.column)}, ${tokenizeColumn(col.column)})"
+        } else {
+          s"splitByChar(char($s), ${tokenizeColumn(col.column)})"
+        }
+      } else {
+        s"splitByString(${tokenizeColumn(sep.column)}, ${tokenizeColumn(col.column)})"
       }
     case SplitByString(sep: StringColMagnet[_], col: StringColMagnet[_]) =>
       s"splitByString(${tokenizeColumn(sep.column)}, ${tokenizeColumn(col.column)})"
