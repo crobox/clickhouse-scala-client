c8acef5342273dc77099cc3f4a49d11842bdf84f
Scala Steward <43047562+scala-steward@users.noreply.github.com>
2025-06-10 20:21:19 +0200

    Update pekko-http to 1.2.0 (#199)

--- project/Build.scala
+++ project/Build.scala
@@ -3,7 +3,7 @@ import sbt._
 object Build {
 
   val PekkoVersion     = "1.1.3"
-  val PekkoHttpVersion = "1.1.0"
+  val PekkoHttpVersion = "1.2.0"
 
   val testDependencies = Seq("org.scalatest" %% "scalatest" % "3.2.19", "ch.qos.logback" % "logback-classic" % "1.5.18")
 }
