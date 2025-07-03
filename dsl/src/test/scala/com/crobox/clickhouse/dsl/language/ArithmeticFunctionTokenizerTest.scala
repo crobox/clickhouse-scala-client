f2430d3bc5e540dfecbaf345e00765c4876c229c
Tayfun Oztemel <tayfun@crobox.com>
2025-07-03 14:14:51 +0200

    add more tests

--- dsl/src/test/scala/com/crobox/clickhouse/dsl/language/ArithmeticFunctionTokenizerTest.scala
+++ dsl/src/test/scala/com/crobox/clickhouse/dsl/language/ArithmeticFunctionTokenizerTest.scala
@@ -17,8 +17,9 @@ class ArithmeticFunctionTokenizerTest extends DslTestSpec {
     toSQL(select(plus(1, 2)), false) should matchSQL("SELECT 1 + 2")
   }
 
-  it should "tokenize add functions" in {
+  it should "tokenize functions with precedence" in {
     toSQL(select(plus(plus(1, 2), 3)), false) should matchSQL("SELECT (1 + 2) + 3")
     toSQL(select(plus(plus(1, 2), divide(3, 4))), false) should matchSQL("SELECT (1 + 2) + (3 / 4)")
+    toSQL(select(divide(multiply(minus(1, 0.9), 100.0), 0.2)), false) should matchSQL("SELECT ((1 - 0.9) * 100.0) / 0.2")
   }
 }
