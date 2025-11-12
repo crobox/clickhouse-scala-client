53327a37dfdbcbf7e4287f228275e95c6b17be83
Scala Steward <scala_steward@virtuslab.com>
2025-11-12 15:19:08 +0000

    Update logback-classic to 1.5.21

--- project/Build.scala
+++ project/Build.scala
@@ -5,5 +5,5 @@ object Build {
   val PekkoVersion     = "1.2.1"
   val PekkoHttpVersion = "1.3.0"
 
-  val testDependencies = Seq("org.scalatest" %% "scalatest" % "3.2.19", "ch.qos.logback" % "logback-classic" % "1.5.20")
+  val testDependencies = Seq("org.scalatest" %% "scalatest" % "3.2.19", "ch.qos.logback" % "logback-classic" % "1.5.21")
 }
