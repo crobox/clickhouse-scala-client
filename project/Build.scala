f36141034f55e0901bee41b40cde6d2aeb9cf9ae
Scala Steward <scala_steward@virtuslab.com>
2025-11-20 14:43:52 +0000

    Update pekko-actor, pekko-stream, ... to 1.3.0

--- project/Build.scala
+++ project/Build.scala
@@ -2,7 +2,7 @@ import sbt._
 
 object Build {
 
-  val PekkoVersion     = "1.2.1"
+  val PekkoVersion     = "1.3.0"
   val PekkoHttpVersion = "1.3.0"
 
   val testDependencies = Seq("org.scalatest" %% "scalatest" % "3.2.19", "ch.qos.logback" % "logback-classic" % "1.5.20")
